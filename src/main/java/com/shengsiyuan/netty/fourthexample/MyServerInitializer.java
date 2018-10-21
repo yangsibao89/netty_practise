0e13c9990cab6c8ba0152b21536e0430b884116e
yangsibao89 <yangsibao89@gmail.com>
2018-10-21 22:48:52 +0800

    7、8、9课

---
+++ src/main/java/com/shengsiyuan/netty/fourthexample/MyServerInitializer.java
@@ -0,0 +1,22 @@
+package com.shengsiyuan.netty.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by yangsibao on 2018/10/21.
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(3, 5, 2, TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
